5acf83e6333cde5fc8c2545fc77a7372baa8b8b0
zhouliangyu <zhouliangyu@huawei.com>
2022-10-28 22:44:03 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.16
+
+require (
+	github.com/Shopify/sarama v1.37.2
+	github.com/astaxie/beego v1.12.3
+	github.com/garyburd/redigo v1.6.4
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/smartystreets/goconvey v1.7.2
+)
---
+++ routers/router.go
@@ -0,0 +1,22 @@
+package routers
+
+import (
+	"main/controllers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
+)
+
+func init() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/seckill/seckill", &controllers.SeckillController{})
+	beego.Router("/seckill/getCount", &controllers.GetCountController{})
+
+}
